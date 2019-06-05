cff392075bbe0a5db7c6257406b6c1c8dabda8ca
Ubuntu <ubuntu@ip-172-31-7-245.us-east-2.compute.internal>
2019-06-05 21:52:56 +0000

    Fixed the library

--- vars/pullDocker.groovy
+++ vars/pullDocker.groovy
@@ -1,8 +1,5 @@
 #!/usr/bin/env groovy
 
-node {
-	
-	def image = docker.image("centos:latest");
-	stage 'Mirror'
-	image.pull()
-}
+def image = docker.image("centos:latest");
+stage 'Mirror'
+image.pull()
